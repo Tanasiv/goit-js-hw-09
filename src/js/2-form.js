1061f1ee4f863f18511bb3085cf3ba71ed218607
Snizhana Tanasiv <snizok.t33@gmail.com>
2026-03-25 01:15:37 +0100

    hw-js-09

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -7,30 +7,39 @@ document.addEventListener('DOMContentLoaded', () => {
   if (!savedFormData) return;
 
   const parsedFormData = JSON.parse(savedFormData);
-  formData.email = parsedFormData.email;
-  formData.message = parsedFormData.message;
+
+  formData.email = parsedFormData.email || '';
+  formData.message = parsedFormData.message || '';
+
   form.elements.email.value = formData.email;
   form.elements.message.value = formData.message;
-})
-
-form.addEventListener('input', (e) => {
-  e.preventDefault();
-  const formInput = new FormData(form);
-  const email = formInput.get('email').trim();
-  const message = formInput.get('message').trim();
-  formData.email = email;
-  formData.message = message;
-  localStorage.setItem(FEEDBACK_FORM_STATE, JSON.stringify(formData));
-
 });
 
+// input
+form.addEventListener('input', (e) => {
+  formData[e.target.name] = e.target.value.trim();
+
+  localStorage.setItem(
+    FEEDBACK_FORM_STATE,
+    JSON.stringify(formData)
+  );
+});
+
+// submit
 form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  if (!formData.email || !formData.message || formData.email === '' || !formData.message === '') {
+  e.preventDefault();
+
+  if (!formData.email || !formData.message) {
     alert('Fill please all fields');
     return;
   }
+
   console.log(formData);
+
   localStorage.removeItem(FEEDBACK_FORM_STATE);
+
+  formData.email = '';
+  formData.message = '';
+
   form.reset();
 });
